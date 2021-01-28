bb4f02000d753ff337421818fbd7899648a97da3
othy1010 <76842861+othy1010@users.noreply.github.com>
2021-01-28 01:13:56 +0100

    Update crypto.c

--- crypto/crypto.c
+++ crypto/crypto.c
@@ -1,66 +1,18 @@
-#include<stdio.h>
-
-int code(char *str,int key)
+int decode (char *str,int key)
 {
-	char ch;
-	int i;
-
-
-	for(i = 0; str[i] != '\0'; ++i){
-		ch = str[i];
-
-		if(ch >= 'a' && ch <= 'z'){
-			ch = ch + key;
-
-			if(ch > 'z'){
-				ch = ch - 'z' + 'a' - 1;
-			}
-
-			str[i] = ch;
-		}
-		else if(ch >= 'A' && ch <= 'Z'){
-			ch = ch + key;
-
-			if(ch > 'Z'){
-				ch = ch - 'Z' + 'A' - 1;
-			}
-
-			str[i] = ch;
-		}
+    int i;
+	for(i = 0; str[i] != '\0'; ++i)
+	{
+	    str[i]-=key;
 	}
-
 	return 0;
 }
-
-int decode(char *str,int key)
+int code (char *str,int key)
 {
-	char ch;
-	int i;
-
-
-	for(i = 0; str[i] != '\0'; ++i){
-		ch = str[i];
-
-		if(ch >= 'a' && ch <= 'z'){
-			ch = ch - key;
-
-			if(ch < 'a'){
-				ch = ch + 'z' - 'a' + 1;
-			}
-
-			str[i] = ch;
-		}
-		else if(ch >= 'A' && ch <= 'Z'){
-			ch = ch - key;
-
-			if(ch < 'A'){
-				ch = ch + 'Z' - 'A' + 1;
-			}
-
-			str[i] = ch;
-		}
+    int i ;
+	for(i = 0; str[i] != '\0'; ++i)
+	{
+	    str[i]+=key;
 	}
-
-
 	return 0;
 }
