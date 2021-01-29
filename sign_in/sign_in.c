505799f479f71d60b729ba268ef478c6f3b03e6c
othy1010 <76842861+othy1010@users.noreply.github.com>
2021-01-29 18:57:57 +0100

    Create sign_in.c

---
+++ sign_in/sign_in.c
@@ -0,0 +1,71 @@
+int key=17;
+
+
+
+int sign_in(char *path,char *mode)//games_mode_name
+{
+    //creat folder
+    char password[30];
+    char name[20];
+    printf("entrer your name and your password :");
+    fscanf(stdin, "%s %s",name,password);
+
+    path=malloc(sizeof(path)+sizeof(mode)+sizeof(name)+1);
+    strcpy(path,"games");
+    strcat(path,"/");
+    strcat(path,mode);
+    strcat(path,"/");
+
+    char user_path[100];
+    strcpy(user_path,path);
+
+    strcat(path,name);
+    if(!creat_folder(path))
+    {
+        printf("name already exist try another name");
+        return 0;
+    }
+
+    //folder created
+    strcat(user_path,"users.txt");
+    FILE *file;
+    file=fopen(user_path,"a");
+
+    //open the user file
+    if (file==NULL)
+    {
+        printf("could,t open the file ");
+        return 0;
+    }
+
+    code(name,key);
+    code(password,key);
+    //append the name and the password
+    fprintf(file,name);
+    fprintf(file,"\n");
+    fprintf(file,password);
+    fprintf(file,"\n////////////////////////////////////////////////////////\n");
+
+    fclose(file);printf("\ndone");
+    return 1;
+
+}
+
+int decode (char *str,int key)
+{
+    int i;
+	for(i = 0; str[i] != '\0'; ++i)
+	{
+	    str[i]-=key;
+	}
+	return 0;
+}
+int code (char *str,int key)
+{
+    int i ;
+	for(i = 0; str[i] != '\0'; ++i)
+	{
+	    str[i]+=key;
+	}
+	return 0;
+}
