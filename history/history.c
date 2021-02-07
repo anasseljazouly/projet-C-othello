f8a9ac637efa89fc5c8a8cefb58e8661c782e263
othy1010 <76842861+othy1010@users.noreply.github.com>
2021-02-07 13:00:40 +0100

    Update history.c

--- history/history.c
+++ history/history.c
@@ -3,10 +3,15 @@
 
 
 
+
 //this function should be add in evry move 
+//**GLOBAL
+char game_name[20];
+char mode[6];
+char player_name[20];
+int save_history_counter=0;
 
-
-void save_history (int pos_l,int pos_c,int player,char *player_name,char *game_name,char *mode)
+void save_history (int pos_l,int pos_c,int player,)
 {
     //Access the file
     char *path[100];
@@ -18,6 +23,7 @@ void save_history (int pos_l,int pos_c,int player,char *player_name,char *game_n
     
     //open the file
     file=fopen(path,"a");
+    fseek(file,-save_history_counter*5,SEEK_END);
     if(file==NULL)
     {
         printf("file didn t open ");
@@ -30,7 +36,8 @@ void save_history (int pos_l,int pos_c,int player,char *player_name,char *game_n
         sprintf(mouvement,"B %c-%d\n",pos_c+65, pos_l);
     if (player==WHITE)
         sprintf(mouvement,"W %c-%d\n",pos_c+65, pos_l);
-
+        
+	increment_decrement(1);
     //close the file
     fprintf(file,mouvement);
     fclose(file);
@@ -38,29 +45,57 @@ void save_history (int pos_l,int pos_c,int player,char *player_name,char *game_n
 }
 
 
+//in move backward area we should add 
+//in move backward increment_decrement(0)
+//in move forward increment_decrement(1)
 
-void reload_history (char *player_name,char *game_name,char *mode)
+void increment_decrement(int i)
+{
+	if (save_history_counter==0 )
+		if(i)
+			save_history_counter++;
+		return;
+	if(i)
+		save_history_counter++;
+	else
+		save_history_counter--;
+	return; 
+		
+}
+
+
+
+
+void reload_history ()
 {
     //Access the file
     char *path[100];
     FILE *file=NULL;
-
+	
+	//ask them to give you the game name
+	fscanf(stdin,"%s",game_name);
+	
     //we get the path 
     sprintf(path,"games/%s/%s/history-%s.txt",player_name,mode,game_name);//why games ? because the main folder which contain the game is named games
     //we should have a path like :games/1vs1/imad/history-imad&hamza.txt
     
     //open the file
-    file=fopen(path,"r");
+    file=fopen(path,"a");
     if(file==NULL)
     {
         printf("file didn t open ");
         return -1;//create or recreate the file
     }
 
+    char buffer[6];
     //save the mouvement 
-    while(feof(file))
+    while(!feof(file))
     {
-        //not completed yet # jani mout n3as
+        fgets(buffer,5,file);
+        strcat(buffer,'\0');
+        
+        //***************put your code here***************
+        //show the buffer in SDl
     }
 
     //close the file
